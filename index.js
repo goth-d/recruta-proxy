7ebbf7d0a958e2bec2657296eed17373865921be
ruansenadev <ruansenadev@gmail.com>
2022-10-25 21:26:58 -0300

    saida requisicoes no console, criação erros http

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
 import { createServer as criarServidor } from "http";
 import { request as requisicao } from "undici";
 import { mesclarCabecalhos } from "./util.js";
+import morgan from "morgan";
+import porFim from "finalhandler";
+import criarErro from "http-errors";
+
+const registrar = morgan(
+  // veja https://www.npmjs.com/package/morgan#tokens
+  ":remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms",
+  // não escreve cors preflight no console
+  { skip: (req) => req.method == "OPTIONS" }
+);
 
 /** A instância do servidor proxy */
 const servidor = criarServidor((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", req.headers['access-control-request-headers'] || "");
+  /** Último tratamento da requisição, **para entregar erros somente** */
+  const terminado = porFim(req, res);
 
-  if (req.method == "OPTIONS") {
-    // cors preflight
-    res.statusCode = 200;
-    res.write("");
-  } else {
-    const endereco = req.headers["endereco-alvo"]; // endereco alvo url <scheme>://<hostname> | <scheme>://<hostname>:<port>
-    if (!endereco) {
-      res.statusCode = 400;
-      res.end(JSON.stringify({ erro: "Não há um endereço no cabeçalho da requisição" }));
-      return;
-    } else delete req.headers["endereco-alvo"]
+  // requisição passa pelo pacote "morgan" para registrar na saída do console
+  registrar(req, res, (erro) => {
+    if (erro) return terminado(erro);
 
-    // TODO: add requisicao log
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", req.headers['access-control-request-headers'] || "");
 
-    requisicao(endereco + req.url, { method: req.method, body: req.body, headers: mesclarCabecalhos(req.headers) })
-      .then((alvoRes) => {
+    if (req.method == "OPTIONS") {
+      // cors preflight
+      res.statusCode = 200;
+      return res.write("");
+    } else {
 
-        res.writeHead(alvoRes.statusCode, alvoRes.headers);
-        alvoRes.body.pipe(res);
-      }).catch((erro) => {
-        console.log(erro);
+      let endereco = req.headers["endereco-alvo"]; // endereco alvo url <scheme>://<hostname> | <scheme>://<hostname>:<port>
+      if (!endereco) {
 
-        res.statusCode = 500;
-        res.end(JSON.stringify({ erro: "Erro durante a requisição, verifique o erro no servidor" }));
-      })
-  }
+        return terminado(criarErro(400, "Não há um endereço no cabeçalho da requisição, defina 'endereco-alvo' com o endereço [<scheme>://<hostname> | <scheme>://<hostname>:<port>] a ser requisitado"));
+      } else {
+
+        if (Array.isArray(endereco)) endereco = endereco[0];
+        delete req.headers["endereco-alvo"];
+      }
+
+      requisicao(endereco + req.url, { method: req.method, body: req.body, headers: mesclarCabecalhos(req.headers) })
+        .then(async (alvoRes) => {
+          // erro de CORS ainda pode ser retornado se a política anti-script do alvo não permitir requisições desta origem
+          
+          // abordagem comum, os navegadores calculam o tamanho ao final caso 'content-length' não esteja definido
+          res.writeHead(alvoRes.statusCode, alvoRes.headers);
+          alvoRes.body.pipe(res);
+
+          // adaptação para prover 'content-length' cabeçalho independente do '.statusCode'
+          // - esta abordagem adiciona tempo de resposta no proxy
+          /* alvoRes.body.arrayBuffer()
+            .then((buff) => {
+              alvoRes.headers["Content-Length"] = Buffer.byteLength(buff, alvoRes.body.readableEncoding);
+              res.writeHead(alvoRes.statusCode, alvoRes.headers);
+              res.write(buff, "binary");
+              res.end(null, "binary");
+            }) */
+        })
+        .catch(terminado)
+    }
+  })
 
 });
 
 /** @type {number} */
-const porta = process.env.PORT || 44;
+const porta = Number(process.env.PORT) || 44;
 
-servidor.listen(Number(porta), () => {
-  console.log("Servidor proxy http/1.1 escutando na porta " + porta);
+servidor.listen(porta, () => {
+  console.log("Servidor proxy escutando na porta " + porta);
 });
 
 // FIXME: lidar com possível erro de runtime no node e prevenir de finalizar o process
